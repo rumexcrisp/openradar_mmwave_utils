a38659391577de5f704891c38cb093de346bc622
Julian Baisch <julian-baisch@web.de>
2024-02-16 18:16:45 +0100

    finish dca part

--- multi_dca.py
+++ multi_dca.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+from time import sleep
 import argparse
 import json
 import os
+import psutil
 import subprocess
 import sys
 import tempfile
@@ -24,6 +26,27 @@ class colors:
     CYAN = '\033[96m'
     WHITE = '\033[97m'
     END = '\033[0m'
+
+
+def printV(string):
+    if args.verbose:
+        print(string)
+
+
+def check_network_address(search_addr):
+    addresses = psutil.net_if_addrs()
+    stats = psutil.net_if_stats()
+    available_networks = {}
+    for intface, addr_list in addresses.items():
+        if any(getattr(addr, 'address').startswith("169.254") for addr in addr_list):
+            continue
+        elif intface in stats and getattr(stats[intface], "isup"):
+            for addr in addr_list:
+                if addr.address == search_addr:
+                    available_networks[intface] = addr.address
+                    break
+
+    return available_networks
 
 
 def start_process(executable, config_file):
@@ -49,7 +72,36 @@ def parse_dca_config(config) -> dict[str, str]:
         ip_update = eth_config_update["DCA1000IPAddress"]
         mac_update = eth_config_update["DCA1000MACAddress"]
         # print(f"ip: {ip} <-> new ip: {ip_update}, mac: {mac_update}")
-        return {"ip": ip, "ip_update": ip_update, "mac_update": mac_update}
+        return {"json": parsed_config, "ip": ip, "ip_update": ip_update, "mac_update": mac_update}
+
+
+def check_ip_route(destination):
+    try:
+        result = subprocess.run(['ip', 'route', 'show', destination], capture_output=True, text=True, timeout=5)
+        if result.returncode == 0 and destination in result.stdout:
+            return True
+        else:
+            return False
+    except subprocess.TimeoutExpired:
+        print("Timeout: Command took too long to execute.")
+        return False
+    except Exception as e:
+        print("Error occurred:", e)
+        return False
+
+
+def run_dca_cmd(cmd, jsonfile, timeout=5):
+    result = 0
+    try:
+        # timeout request to cut waiting time
+        result = subprocess.run([executables['dca'], cmd, jsonfile], capture_output=True, timeout=timeout, check=True)
+        result = result.returncode
+    except subprocess.TimeoutExpired:
+        result = 2
+    except subprocess.CalledProcessError:
+        print(f"{executables['dca']} returned error")
+        result = 1
+    return result
 
 
 def main(config_files, executables):
@@ -65,45 +117,80 @@ def setup(config_files, executables):
     print("Running setup routine\n")
     print(f"Found {len(config_files)} config files: {config_files}")
     print(f"Going to setup {len(config_files)} radar modules:")
-    for i, config in enumerate(config_files):
-        parsed_dict = parse_dca_config(config)
-        print(f"{colors.CYAN}Radar {i+1}{colors.END}: Please connect a single radar which should be assigned the ip adress: {parsed_dict['ip_update']}")
-        input(" Press enter when connected...")
+    reprogrammed = False
+    for i, config_file_path in enumerate(config_files):
+        parsed_dict = parse_dca_config(config_file_path)
+        print(f"{colors.CYAN}Radar {i+1}{colors.END}: Please connect a single radar which should be assigned the ip address: {parsed_dict['ip_update']}")
+        if input(" Press enter when DCA ethernet is connected or 'c' to cancel...") == 'c':
+            continue
 
-        with tempfile.TemporaryDirectory() as temp_dir:
-            # create temp config file to change ip and test for dca reachability
-            with tempfile.NamedTemporaryFile(dir=temp_dir, prefix="dca_config_", suffix=".json") as temp_config:                
-                ip_tmp = parsed_dict['ip']
-                config_temp = config
-                ip_found = None
-                print("Checking DCA reachability, please be patient...")
-                for i in range(11):
-                    # dump json to temp config and test
-                    json.dump(config_temp, temp_config)
-                    try:
-                        result = subprocess.run([executables['dca'], "reset_fpga", temp_config], capture_output=True, timeout=0.5)
-                        if result.returncode == 0:
-                            print(f"found dca on ip: {ip_tmp}")
-                            ip_found = ip_tmp
-                            break
-                    except subprocess.TimeoutExpired:
-                        continue
-                    ip_tmp = ip_tmp.split(".")
-                    ip_tmp[-1] = str(int(ip_tmp[-1]) + 1)
-                    ip_tmp = ".".join(ip_tmp)
-                    config_temp["DCA1000Config"]["ethernetConfig"]["DCA1000IPAddress"] = ip_tmp
+        # with tempfile.TemporaryDirectory(prefix="setup_dca_1000_") as temp_dir:
+        fixed_temp_dir = os.path.join(tempfile.gettempdir(), "setup_dca_1000")
+        if not os.path.exists(fixed_temp_dir):
+            os.makedirs(fixed_temp_dir)
 
-                if ip_found is None:
-                    print("Could not reach DCA, check connection")
+        temp_file_path = os.path.join(fixed_temp_dir, f"dca_cf.json")
+        # always start at base ip
+        ip_tmp = "192.168.33.180"
+        config_temp = parsed_dict['json']
+        config_temp["DCA1000Config"]["ethernetConfig"]["DCA1000IPAddress"] = ip_tmp
+        ip_found = None
+        print("Checking DCA reachability, please be patient...")
+        for i in range(11):
+            # dump json to temp config and test
+            with open(temp_file_path, 'w') as temp_file:
+                json.dump(config_temp, temp_file, indent=2)
+            printV(f"IP: {ip_tmp}")
+            if len(check_network_address("192.168.33.30")) == 0:
+                print("Waiting for network with fixed ip 192.168.33.30...")
+            while len(check_network_address("192.168.33.30")) == 0:
+                sleep(1)
+            result = run_dca_cmd("reset_fpga", temp_file_path, timeout=2)
+            if result == 0:
+                print(f"found dca on ip: {ip_tmp}")
+                ip_found = ip_tmp
+                break
+            else:
+                pass
+            ip_tmp = ip_tmp.split(".")
+            ip_tmp[-1] = str(int(ip_tmp[-1]) + 1)
+            ip_tmp = ".".join(ip_tmp)
+            config_temp["DCA1000Config"]["ethernetConfig"]["DCA1000IPAddress"] = ip_tmp
+        if ip_found is None:
+            print("Could not reach DCA, check connection or repower")
+            continue
 
-        # print(f"{colors.CYAN}Radar {i}{colors.END}: Please disconnect the radar")
-        # input(" Press enter when disconnected...")
+        if ip_tmp != parsed_dict['ip_update'] or args.force:
+            # create update json and program dca
+            config_temp["DCA1000Config"]["ethernetConfigUpdate"]["DCA1000IPAddress"] = parsed_dict['ip_update']
+            with open(temp_file_path, 'w') as temp_file:
+                json.dump(config_temp, temp_file, indent=2)
+            result = run_dca_cmd("eeprom", temp_file_path)
+            if result == 0:
+                run_dca_cmd("reset_fpga", temp_file_path, timeout=2)
+                reprogrammed = True
+                print(f"reprogrammed dca to ip: {parsed_dict['ip_update']}")
+                config_temp["DCA1000Config"]["ethernetConfig"]["DCA1000IPAddress"] = parsed_dict['ip_update']
+                with open(config_file_path, 'w') as orig_config_file:
+                    json.dump(config_temp, orig_config_file, indent=2)
+            else:
+                print("Error reprogramming")
+                printV(result.stdout)
+                printV(result.stderr)
+        else:
+            print("IPs match, nothing todo, continuing...")
+
+        if i < len(config_files):
+            input(" Press enter when DCA ethernet disconnected...")
+    if reprogrammed:
+        print("\nRepower the radar module(s) to finish programming.")
 
 
 if __name__ == "__main__":
     parser = MyParser(description='mmWave Radar Wrapper')
     parser.add_argument('setup', help='Run setup routine', nargs='?')
     parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode')
+    parser.add_argument('-f', '--force', action='store_true', help='Force')
     args = parser.parse_args()
 
     executables = {"dca": "bin/setup_dca_1000", "radar": "bin/setup_radar"}
